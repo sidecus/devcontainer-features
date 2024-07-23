085319bd5e40181b580caa9a524e4fc50249cd81
sidecus <4399408+sidecus@users.noreply.github.com>
2024-07-23 13:14:04 +0800

    try and error

--- test/magicmirror/hf.sh
+++ test/magicmirror/hf.sh
@@ -11,7 +11,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 # check "test HF mirror" bash -c "test \"${HF_ENDPOINT}\" = \"https://dummy-hf-mirror.com\""
-check "test HF mirror" bash -c "echo \"${HF_ENDPOINT}\"; echo /etc/environment;"
+check "test HF mirror" bash -c "echo \"${HF_ENDPOINT}\"; cat /etc/environment;"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
